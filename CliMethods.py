ce219af95a242d05aa68e137a54372a4f5e839d4
Jake Klein <jakeklein91@gmail.com>
2021-02-22 22:48:44 +0200

    Added files, added serverside template and Cli methods

---
+++ CliMethods.py
@@ -0,0 +1,7 @@
+def parse_cli(cli_command_raw):
+    pass
+
+
+def commit_handle(commit_message=""):
+    pass  # create file that contains the metadata for commits
+
---
+++ serverSideSimulation.py
@@ -0,0 +1,10 @@
+import socket
+import MidiMethods
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
