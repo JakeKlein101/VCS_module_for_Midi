096a8a37e8e380cbcc235de3a213273f81910207
Jake Klein <jakeklein91@gmail.com>
2021-03-13 16:52:49 +0200

    Added configuration file parsing when using a command that is not init.

--- gitbit.py
+++ gitbit.py
@@ -7,15 +7,15 @@ parser.add_argument("opcode", type=str, help="Choose an opcode that suits the wa
                                              " for example: commit, push, init.")
 parser.add_argument("-o", "--opcode_args", type=str, help="Optional arguments for certian functions.")
 
+# TODO: Research about how to make only specific arguments have optional parameters with the argpare library.
+
 
 def main():
     args = parser.parse_args()
     if args.opcode == "commit":
         utils.handle_commit(args.opcode_args)
     elif args.opcode == "init":
-        utils.handle_init(args.opcode_args)
-    elif args.opcode == "end_repo":
-        utils.handle_end_repo(args.opcode_args)
+        utils.handle_init()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -8,42 +8,50 @@ import json
 FILE_ATTRIBUTE_HIDDEN = 0x02
 REPO_PATH = ""
 
-# Functions:
+# Utility functions:
+
+
+def conf_parse():
+    """
+    Used on all occasitons except init. Used to open the configuration file and set up the needed data
+    about the repository.
+    """
+    global REPO_PATH
+
+    with open(os.path.join(os.getcwd(), ".gitbit", "conf.json"), "r") as conf_file:
+        conf_content = json.load(conf_file)
+        REPO_PATH = conf_content["repo_data"]["repo_path"]
+        print("path: " + REPO_PATH)
+
+# Argument handlers:
 
 
 def handle_commit(commit_message):  # TODO: Research and find a way to make reversable commits.
+    conf_parse()
     global REPO_PATH
-    if not os.path.exists(REPO_PATH):  # TODO: Parse the conf file.
+    if not os.path.exists(REPO_PATH):
         print("repo path" + REPO_PATH)
         print("No repository found.")
     else:
         print("Commit message: ", commit_message)  # create file that contains the metadata for commits
 
 
-def handle_init(path):
+def handle_init():
     global REPO_PATH
     conf_json = {}
-    if path:
-        REPO_PATH = os.path.join(path, ".gitbit")
-    else:
-        REPO_PATH = os.path.join(os.getcwd(), ".gitbit")
+    REPO_PATH = os.path.join(os.getcwd(), ".gitbit")
 
     if not os.path.exists(REPO_PATH):
         os.mkdir(REPO_PATH)
         ctypes.windll.kernel32.SetFileAttributesW(REPO_PATH, FILE_ATTRIBUTE_HIDDEN)
 
-        conf_json["repo_data"] = []
-        conf_json["repo_data"].append(
-            {'repo_path': REPO_PATH}
-        )
+        conf_json["repo_data"] = {"repo_path": REPO_PATH}
+
         with open(os.path.join(REPO_PATH, "conf.json"), "w") as conf_file:
             json.dump(conf_json, conf_file)
     else:
         print("There is already a repository in this working directory.")
 
-
-def handle_end_repo(args):
-    pass  # TODO: deletes the repository and creates a file that will delete the remote origin on push.
 
 
 
