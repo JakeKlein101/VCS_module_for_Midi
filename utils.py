25936425318d2d3dfaca09dd9ef3cf365646cceb
Jake Klein <jakeklein91@gmail.com>
2021-03-28 15:13:42 +0300

    Added commit management. The repo always keeps the latest 5 commit dir's.

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 import os
 import ctypes
 import json
-import diff_module
 
 # consts:
 
 FILE_ATTRIBUTE_HIDDEN = 0x02
 REPO_PATH = ""
+COMMIT = 0
+
 
 # Utility functions:
-
 
 def conf_parse():
     """
@@ -17,21 +17,63 @@ def conf_parse():
     about the repository.
     """
     global REPO_PATH
+    global COMMIT
 
     with open(os.path.join(os.getcwd(), ".gitbit", "conf.json"), "r") as conf_file:
         conf_content = json.load(conf_file)
-        REPO_PATH = conf_content["repo_data"]["repo_path"]
-        print("path: " + REPO_PATH)
+        REPO_PATH = conf_content["repo_path"]
+        COMMIT = conf_content["commit_count"]
+        print("commit:", COMMIT)
+
+
+def conf_update():
+    """
+    Used on all occasions except init.
+    Serves as a closing function to "conf_parse".
+    It updates the configuration file with the changes that have been made when the program was executed.
+    """
+    global REPO_PATH
+    global COMMIT
+
+    with open(os.path.join(os.getcwd(), ".gitbit", "conf.json"), "r") as conf_file:
+        conf_content = json.load(conf_file)
+
+        # The changes to the configuration file are done here:
+        conf_content["commit_count"] += 1
+
+    with open(os.path.join(os.getcwd(), ".gitbit", "conf.json"), "w") as conf_file:
+        json.dump(conf_content, conf_file)
+
+
+def delete_fifth_last():
+    """
+    Used to keep commit amount managable. It always keeps the latest 5 commits in the repository for rollback options.
+    """
+    global COMMIT
+    global REPO_PATH
+
+    path_to_delete = os.path.join(REPO_PATH, "commit " + str(COMMIT - 5))
+    print(path_to_delete)
+    os.rmdir(path_to_delete)
 
 # Argument handlers:
 
 
 def handle_commit(commit_message):
-    conf_parse()
+    conf_parse()  # Extracts the data from the configuration file into global variables before operation.
     global REPO_PATH
+    global COMMIT
+
     if os.path.exists(REPO_PATH):
-        print("Commit message: ", commit_message)
         # TODO: enter the current and previous file into the commit system and make comparisons.
+
+        print("Commit message: ", commit_message)
+        if COMMIT >= 5:
+            delete_fifth_last()
+
+        os.mkdir(os.path.join(REPO_PATH, "commit " + str(COMMIT)))
+        COMMIT += 1
+        conf_update()
     else:
         print("repo path" + REPO_PATH)
         print("No repository found.")
@@ -39,6 +81,8 @@ def handle_commit(commit_message):
 
 def handle_init():
     global REPO_PATH
+    global COMMIT
+
     conf_json = {}
     REPO_PATH = os.path.join(os.getcwd(), ".gitbit")
 
@@ -46,7 +90,8 @@ def handle_init():
         os.mkdir(REPO_PATH)
         ctypes.windll.kernel32.SetFileAttributesW(REPO_PATH, FILE_ATTRIBUTE_HIDDEN)
 
-        conf_json["repo_data"] = {"repo_path": REPO_PATH}
+        conf_json["repo_path"] = REPO_PATH
+        conf_json["commit_count"] = COMMIT
 
         with open(os.path.join(REPO_PATH, "conf.json"), "w") as conf_file:
             json.dump(conf_json, conf_file)
