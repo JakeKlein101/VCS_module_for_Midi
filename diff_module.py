fcd4d3935fc16d0956d66797e7076e5d54ae45d7
Jake Klein <jakeklein91@gmail.com>
2021-04-28 16:56:18 +0300

    Changed the format of logging differences to the changes file.

--- diff_module.py
+++ diff_module.py
@@ -16,15 +16,14 @@ def file_paths_to_midi_objects(latest_file_path, second_to_last_file_path):
     return latest_file, second_to_last_file
 
 
-def log_diffs_to_file(latest_file_path, latest_file_ms, second_to_last_file_ms, message_index):
+def log_diffs_to_file(latest_file_path, latest_file_ms, track_index, message_index):
     """
-    Logs the different messages that were found between the MIDI files that were compared to a changes.gitbit file.
-    The file contains the message index, followed by the new version of the message
-     that is followed by the message from the second to latest commit.
+    Logs the diffences that were found between the MIDI files to a changes.gitbit file.
+    The format of the loggin in the file is as followed:
+    Track index of diff:Message index of diff:Diff message.
     """
     with open(os.path.join(os.path.dirname(latest_file_path), "changes.gitbit"), "a") as changes_log_file:
-        changes_log_file.write(str(message_index) + ": " + str(latest_file_ms)
-                               + "\n" + str(second_to_last_file_ms) + "\n")
+        changes_log_file.write(str(track_index) + ":" + str(message_index) + ":" + str(latest_file_ms) + "\n")
 
 
 # Main method:
@@ -36,19 +35,23 @@ def main_diff(latest_file_path, second_to_last_file_path):  # TODO: check all ed
 
     latest_file, second_to_last_file = file_paths_to_midi_objects(latest_file_path, second_to_last_file_path)
 
-    for latest_file_track, second_to_last_file_track in zip(latest_file.tracks, second_to_last_file.tracks):
+    for track_index, packed_track_args in enumerate(zip(latest_file.tracks, second_to_last_file.tracks)):
+        latest_file_track, second_to_last_file_track = packed_track_args
+
         # Iterates and compares the tracks in the files, if the tracks are equal, there is no need to iterate over them.
+        print(f"Checking track {track_index}:")
         if latest_file_track == second_to_last_file_track:
+            print(f"Tracks NO. {track_index} have no difference.")
             continue
         else:
             # Iterates and compares each message over unequal tracks, prints True over equals and false over diffs.
-            # TODO: Add the track index to the log file.
+            print(f"Checking track messages for track  {track_index}:")
             for message_index, packed_args in enumerate(zip(latest_file_track, second_to_last_file_track)):
                 latest_file_ms, second_to_last_file_ms = packed_args
                 print(str(message_index) + " Latest: " + str(latest_file_ms)
                       + " Second Latest: " + str(second_to_last_file_ms))
                 if latest_file_ms != second_to_last_file_ms:
-                    log_diffs_to_file(latest_file_path, latest_file_ms, second_to_last_file_ms, message_index)
+                    log_diffs_to_file(latest_file_path, latest_file_ms, track_index, message_index)
                     print(False)
                 else:
                     print(True)
